21c377a242726c9ad850b9670cd65c63b3129369
aprutledge <39936030+aprutledge@users.noreply.github.com>
2021-03-02 22:53:32 -0600

    Updates

---
+++ precache-manifest.0e0f3827089013632ce20c007571a438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4acdfd3bad59580995c0886e788ad690",
+    "url": "/my-react-portfolio-site/index.html"
+  },
+  {
+    "revision": "19ab7d3b206970af265b",
+    "url": "/my-react-portfolio-site/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "8acfe051a95329ddfb9c",
+    "url": "/my-react-portfolio-site/static/css/main.835df00f.chunk.css"
+  },
+  {
+    "revision": "19ab7d3b206970af265b",
+    "url": "/my-react-portfolio-site/static/js/2.c46e3027.chunk.js"
+  },
+  {
+    "revision": "5f1132900def3f12ce02e69f13fa4da8",
+    "url": "/my-react-portfolio-site/static/js/2.c46e3027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acfe051a95329ddfb9c",
+    "url": "/my-react-portfolio-site/static/js/main.9e0b99ab.chunk.js"
+  },
+  {
+    "revision": "06a3686456da07bbfc7f",
+    "url": "/my-react-portfolio-site/static/js/runtime-main.7fbc6641.js"
+  }
+]);
